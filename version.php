4a1260ca7830350c3c873e67be658eb84516f94f
Onno Schuit <onno@solin.nl>
2024-11-19 16:19:08 +0100

    Including maturity and release plugin info

--- version.php
+++ version.php
@@ -31,3 +31,5 @@ $plugin->version   = 2024090304;        // The current plugin version (Date: YYY
 $plugin->requires  = 2024042201;        // Requires this Moodle version.
 $plugin->component = 'format_drip';    // Full name of the plugin (used for diagnostics).
 $plugin->name      = 'Drip Format by Solin';
+$plugin->release   = '4.4';
+$plugin->maturity  = MATURITY_STABLE;
